5a6fd0ec9bad7bd4664896fec97669a36b14640f
kwonws <kevincool6@naver.com>
2025-01-14 23:32:54 +0900

    프로젝트 생성

--- MovieAPI/ViewController.swift
+++ MovieAPI/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /*
+     생성한 프로젝트(코드베이스 학습 과제)에 연이어서 복습을 진행
+     - 영화진흥위원회 API를 통해 어제 날짜 기준으로 일간 박스오피스 정보를 보여
+     주는 예제입니다.
+     - 첫 빌드 시 어제 날짜를 보여주고, 텍스트필드에 yyyyMMdd 형식으로 검색을
+     통해 원하는 날짜에 해당하는 박스오피스 정보를
+     조회해봅니다.
+     [Easy]
+     -
+     뷰컨트롤러에 레이블 9개를 얹이고, 1위부터 3위에 해당하는 영화를 “순위, 영
+     화 제목, 개봉일” 3가지를 표현해줍니다.
+     rank순위
+     movieNm 제목
+     openDt 개봉일
+     http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=07918ad2a80648eb7bd0d5fb50437098&targetDt=20120101
+     
+     
+     요청 방식 Get방식으로 호출
+     targetDt를 yyyyMMdd로 입력받아서 넣으면 될것같디
+  
+     */
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
     }
 
+    
+    
+    
+    
+    
+    
 
 }
 
