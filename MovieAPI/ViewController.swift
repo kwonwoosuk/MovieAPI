fdfa96c535827c6dbcff34ce8facbe898503a615
kwonws <kevincool6@naver.com>
2025-01-14 20:31:56 +0900

    Initial Commit

---
+++ MovieAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAPI
+//
+//  Created by 권우석 on 1/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
